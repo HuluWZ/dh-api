4bf6ba8b8a21df5e7ad9825bacba4cd26252d38e
HuluWZ <hulunlante.w@gmail.com>
2024-09-03 21:41:46 +0300

    chore: Update database and Minio configurations to use environment variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 export default () => ({
   database: {
-    host: process.env.POSTGRES_HOST || 'db',
-    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-    username: process.env.POSTGRES_USER || 'postgres',
-    password: process.env.POSTGRES_PASSWORD || 'password',
-    database: process.env.POSTGRES_DB || 'dh',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   },
 });
