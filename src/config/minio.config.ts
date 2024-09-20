3ef7981b00163ab376313de904d2da3f48bd4256
HuluWZ <hulunlante.w@gmail.com>
2024-09-20 23:01:17 +0300

    refactor: Update Minio configuration to use environment variables

--- src/config/minio.config.ts
+++ src/config/minio.config.ts
@@ -1,12 +1,12 @@
 export default () => ({
   minio: {
-    ENDPOINT: process.env.MINIO_ENDPOINT || 'localhost',
-    PORT: parseInt(process.env.MINIO_PORT, 10) || 9000,
+    ENDPOINT: process.env.MINIO_ENDPOINT,
+    PORT: parseInt(process.env.MINIO_PORT, 10),
     USE_SSL: process.env.MINIO_USE_SSL === 'true',
-    ACCESS_KEY: process.env.MINIO_ROOT_USER || 'minio',
-    SECRET_KEY: process.env.MINIO_ROOT_PASSWORD || 'minio123',
-    PUBLIC_BUCKET: process.env.MINIO_PUBLIC_BUCKET || 'public',
-    PRIVATE_BUCKET: process.env.MINIO_PRIVATE_BUCKET || 'private',
+    ACCESS_KEY: process.env.MINIO_ROOT_USER,
+    SECRET_KEY: process.env.MINIO_ROOT_PASSWORD,
+    PUBLIC_BUCKET: process.env.MINIO_PUBLIC_BUCKET,
+    PRIVATE_BUCKET: process.env.MINIO_PRIVATE_BUCKET,
   },
 });
 
