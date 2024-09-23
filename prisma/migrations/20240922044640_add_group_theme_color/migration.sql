22affee4cfaa9dc2e1bebedf23a75422ca13afb9
HuluWZ <hulunlante.w@gmail.com>
2024-09-23 09:02:21 +0300

    feat: include theme option in org group

---
+++ prisma/migrations/20240922044640_add_group_theme_color/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "OrgGroup" ADD COLUMN     "color" TEXT;
--- src/org-group/dto/org-group.dto.ts
+++ src/org-group/dto/org-group.dto.ts
@@ -6,6 +6,14 @@ export class CreateOrgGroupDto {
   @IsString()
   name: string;
 
+  @ApiProperty({
+    example: 'Green Or RGB Code',
+    description: 'Org Group Theme or Color',
+  })
+  @IsString()
+  @IsOptional()
+  color?: string;
+
   @ApiProperty({ example: 1, description: 'Org Id' })
   @IsInt()
   orgId: number;
@@ -16,4 +24,12 @@ export class UpdateOrgGroupDto {
   @IsString()
   @IsOptional()
   name: string;
+
+  @ApiProperty({
+    example: 'Green Or RGB Code',
+    description: 'Org Group Theme or Color',
+  })
+  @IsString()
+  @IsOptional()
+  color?: string;
 }
