2b2b2a67dd65c97f25cea6cf31f3e8849bd0e609
HuluWZ <hulunlante.w@gmail.com>
2024-10-08 14:06:49 +0300

    fix: remove minio service from module and docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommonModule } from './common/common.module';
 import { ConfigAppModule } from './config/config.module';
-import { MinioModule } from './minio/minio.module';
 import { OrgModule } from './org/org.module';
 import { OrgInviteModule } from './org-invite/org-invite.module';
 import { OrgMemberModule } from './org-member/org-member.module';
@@ -24,7 +23,6 @@ import { NotificationModule } from './notification/notification.module';
     OrgInviteModule,
     OrgMemberModule,
     OrgGroupModule,
-    MinioModule,
     OrgGroupMembersModule,
     TaskModule,
     CloudinaryModule,
--- src/minio/minio.config.ts
+++
@@ -1,8 +0,0 @@
-export const minioConfig = {
-  endPoint: process.env.MINIO_ENDPOINT,
-  port: parseInt(process.env.MINIO_PORT, 10),
-  useSSL: false,
-  accessKey: process.env.MINIO_ROOT_USER,
-  secretKey: process.env.MINIO_ROOT_USER,
-};
-export const DEFAULT_BUCKET = process.env.MINIO_BUCKET;
