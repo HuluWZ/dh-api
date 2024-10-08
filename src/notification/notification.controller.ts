f9e5c76fffcf723b94524d9fe8158cd06aee1387
HuluWZ <hulunlante.w@gmail.com>
2024-10-08 20:50:03 +0300

    feat: Add Notification model and migration

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Query,
+  Patch,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
   MultipleDeviceNotificationDto,
   NotificationDto,
 } from './dto/notification.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @ApiTags('Notifications')
 @Controller('notifications')
@@ -12,9 +24,39 @@ export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post('send-notification')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @ApiOperation({ summary: 'Send a push notification to a single device' })
-  async sendNotification(@Body() notificationData: NotificationDto) {
-    return this.notificationService.sendNotification(notificationData);
+  async sendNotification(
+    @Req() req: any,
+    @Body() notificationData: NotificationDto,
+  ) {
+    const userId: number = req.user.id;
+    return this.notificationService.sendNotification(notificationData, userId);
+  }
+
+  @Get('my-notifications')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary:
+      'Get all My notifications set seen to one of [all, true and false] value',
+  })
+  async getMyNotifications(@Req() req: any, @Query('seen') seen: string) {
+    const seenValue =
+      seen.toLowerCase() === 'all'
+        ? null
+        : seen.toLowerCase() === 'true'
+          ? true
+          : false;
+    return this.notificationService.getMyNotifications(1, seenValue);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get Notification By Id' })
+  @UseGuards(AuthGuard)
+  async getNotificationById(@Param('id') id: string) {
+    return this.notificationService.getNotificationById(+id);
   }
 
   @Post('send-multiple-notifications')
@@ -25,5 +67,20 @@ export class NotificationController {
     return this.notificationService.sendNotificationToMultipleTokens(
       multipleNotificationData,
     );
+  }
+  @Patch('id')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Set Seen Notification Status' })
+  async updateNotificationSeenStatus(@Param('id') id: string) {
+    const notification =
+      await this.notificationService.getNotificationById(+id);
+    if (!notification) {
+      throw new NotFoundException(`Notification with id #${id} not found!`);
+    }
+    if (notification.is_seen) {
+      return notification;
+    }
+    return this.notificationService.updateNotificationStatus(+id);
   }
 }
