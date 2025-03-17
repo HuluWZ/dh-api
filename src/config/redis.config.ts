c1cd0fbb69af12180ffbbe3fdb1e3fd47ccdefde
HuluWZ <hulunlante.w@gmail.com>
2025-03-17 10:55:16 +0300

    refactor Redis configuration to remove password

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -2,11 +2,9 @@ export default () => ({
   redis: {
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT, 10),
-    password: process.env.REDIS_PASSWORD,
   },
 });
 export type redisConfigType = {
   host: string;
   port: number;
-  password: string;
 };
